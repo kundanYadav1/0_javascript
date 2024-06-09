2ecaf12b806f9e19d453027b9f21f127a0b57b82
Kundan Prasad Yadav <110804293+kundanYadav1@users.noreply.github.com>
2024-06-09 11:02:48 +0000

    variable declaration

---
+++ basic/0_variable.js
@@ -0,0 +1,18 @@
+const accountId = 1435
+let accountEmail = "kundanprasadyadav15@gmail.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+let accountState;
+// accountId = 2 
+
+accountEmail = "ny88119@gmail.com"
+accountPassword = "7567"
+accountCity = "Delhi"
+console.log(accountId);
+
+console.table([accountId,accountEmail, accountPassword, accountCity, accountState]);
+
+/*
+Alwayes Prefer to use let instead of var
+Because of issue in block scope and functional scope
+*/
