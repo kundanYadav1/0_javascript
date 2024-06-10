05073df473f93ee1d9154d1f9f4b10ab689ce7c9
Kundan Prasad Yadav <110804293+kundanYadav1@users.noreply.github.com>
2024-06-10 09:10:21 +0000

    create_object 1st time

---
+++ obj/01.js
@@ -0,0 +1,12 @@
+const person = {
+    firstName: 'john',
+    lastName: 'DOE',
+    age: 30,
+    isEmployed: true,
+    greet: function(){
+        console.log("Hello")
+    }
+};
+// Object:
+// Objectname.persontyname
+console.log(person.greet())
